1c956c023f5d4b8ab351ac0af76f2dfe193efd4a
Thiago <itsadeadh2@gmail.com>
2024-07-04 18:31:07 -0400

    Update logger

--- src/infrastructure/logger/__init__.py
+++ src/infrastructure/logger/__init__.py
@@ -3,22 +3,7 @@ from logging import StreamHandler
 
 
 def create_logger():
-    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-
-    # Get the root logger
     logger = logging.getLogger()
-
-    # Clear existing handlers
-    if logger.hasHandlers():
-        logger.handlers.clear()
-
-    # Create a StreamHandler to log to console
-    handler = StreamHandler()
-
-    # Create a formatter and set it for the handler
-    formatter = logging.Formatter(log_format)
-    handler.setFormatter(formatter)
-    handler.setLevel(logging.INFO)
-
-    # Set the logging level for the logger
     logger.setLevel(logging.INFO)
+    return logger
+
